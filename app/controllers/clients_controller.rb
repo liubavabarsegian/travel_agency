681c1db101e4bc96bbf066164068cbee8b2e0eaf
liubavabarsegian <lyubahemmo@mail.ru>
2023-03-08 17:08:49 +0300

    filled clients table

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,4 +8,16 @@ class ClientsController < ApplicationController
         sql = params[:select]
         @records_array =  ActiveRecord::Base.connection.exec_query(sql)
     end
+
+    def add_data
+        (0...100).each do 
+            name = Faker::Name.name_with_middle
+            phone = Faker::PhoneNumber.phone_number_with_country_code
+            bday = Faker::Date.birthday
+            has_member_card = [true, false].sample
+            Client.create!({name: name, phone: phone, birthday_date: bday, has_member_card: has_member_card})
+        end
+
+        redirect_to clients_show_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
   get 'trips/show'
 
   post 'clients/query'
+  post 'clients/add_data'
   post 'workers/query'
   post 'hotels/query'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
